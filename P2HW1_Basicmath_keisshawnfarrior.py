f1b03476d2ee68fa958d9c0147c8e43b1f23c180
farriork5780 <67410762+farriork5780@users.noreply.github.com>
2020-06-25 04:02:34 -0400

    Add files via upload
    First submission

---
+++ P2HW1_Basicmath_keisshawnfarrior.py
@@ -0,0 +1,12 @@
+#program that does math
+#6-19-20
+#CTI - 110 P2HW1
+#keisshawn farrior
+num1 = float(input("enter the first number:"))
+num2 = float(input("enter the second number:"))
+sum = num1+ num2
+mul=num1*num2
+print("the sum of the number is:",sum)
+print("the product of given numbers is:",mul)
+#display the sum
+#display the product
